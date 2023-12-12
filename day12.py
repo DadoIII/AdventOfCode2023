1f90591dd04e1362f06a3bd56404a0aa67c46d5e
DadoIII <daniel.zelinka9@gmail.com>
2023-12-13 00:43:47 +0100

    Finnished validating spring groups with a list of numbers

--- day12.py
+++ day12.py
@@ -1,4 +1,4 @@
-inputFile = 'inputs/input12.txt'
+inputFile = 'inputs/test12.txt'
 
 # Part 1
 
@@ -10,7 +10,7 @@ def create_groupings(springs, groups):
 # Recursive function that tries all combinations of spring groups with the group numbers 
 def all_combinations(spring_groups, numbers, pairing):
     current_spring_group = spring_groups[0]
-    # Last spring group. Have to try to fit all the remaining groups in
+    # Last spring group. Have to try to fit all the remaining numbers in it
     if len(spring_groups) == 1:
         if len(current_spring_group) >= sum(numbers) + len(numbers) - 1:
             pairing[current_spring_group] = numbers
@@ -19,7 +19,7 @@ def all_combinations(spring_groups, numbers, pairing):
             return 0
     else:
         total = 0
-        # Try to put different amounts of group numbers into the latest spring group
+        # Try to put different amounts of group numbers into the current spring group
         for i in range(len(numbers)):
             numbers_slice = numbers[:i]
             if len(current_spring_group) >= sum(numbers_slice) + len(numbers_slice) - 1:
@@ -31,10 +31,41 @@ def all_combinations(spring_groups, numbers, pairing):
 
 
 def valid_combinations(pairing):
-    for (springs, numbers) in pairing:
-        pass
-    return 0
+    prod = 1
+    for (springs, numbers) in zip(pairing, pairing.values()):
+        if numbers != []:
+            prod *= all_valid_combinations(springs, numbers, sum(numbers) - springs.count('#'), [], 0)
+                
+    return prod
 
+def all_valid_combinations(springs: str, numbers: [int], springs_to_place: int, placed_indexes: [int], index: int) -> int:
+    # All springs placed, check if the placement is valid
+    if springs_to_place == 0:
+        return 1 if is_valid(springs, numbers, placed_indexes) else 0
+    # Not enough unkown spaces to place the springs
+    elif springs_to_place > springs.count('?') - index:
+        return 0
+    else:
+        total = 0
+        total += all_valid_combinations(springs, numbers, springs_to_place, placed_indexes, index + 1)  # Continue without placing a spring on this index
+        total += all_valid_combinations(springs, numbers, springs_to_place - 1, placed_indexes + [index], index + 1)  # Continue with placing a spring on this index
+        return total
+
+   
+def is_valid(springs, numbers, placed_indexes):
+    q_index = 0
+    for i in range(len(springs)):
+        if springs[i] == '?':
+            if q_index in placed_indexes:
+                springs = springs[:i] + '#' + springs[i+1:]
+            q_index += 1
+    if [len(group) for group in springs.split('?') if '#' in group] == numbers:
+        print("Returning 1", springs, numbers)
+        return 1
+    else:
+        print("Returning 0", springs, numbers)
+        return 0
+        
 
 total = 0
 with open(inputFile, 'r') as file:
@@ -42,10 +73,12 @@ with open(inputFile, 'r') as file:
     for line in file.readlines():
         springs, groups = line.strip().split(" ")
         spring_groups, groups = create_groupings(springs, [int(x) for x in groups.split(",")])
-        print({group: [] for group in spring_groups})
-        total += all_combinations(spring_groups, groups, {group: [] for group in spring_groups})
-        if i > 5:
-            break
+        print(spring_groups, groups)
+        temp = all_combinations(spring_groups, groups, [{group: []} for group in spring_groups])
+        print(temp)
+        total += temp
+        #if i > 5:
+        #    break
         i += 1
 
 print('Part 1:', total)
